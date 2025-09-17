6ca968ad2e51651838ff9d767f301f4915d6d600
Roland Szabo <szabo.roland.1213@gmail.com>
2025-09-17 13:25:58 +0300

    Initial commit.

---
+++ backend/src/main/java/dev/roland/billing_program/BillingProgramApplication.java
@@ -0,0 +1,13 @@
+package dev.roland.billing_program;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingProgramApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/roland/billing_program/BillingProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.roland.billing_program;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
