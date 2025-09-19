b36dfa40c6b30f9338cf0bd3d07f43baa086b736
Roland Szabo <szabo.roland.1213@gmail.com>
2025-09-19 11:59:31 +0300

    [BR-001] Set up of database and base models
    - Created docker-compose for db
    - Created tables and inserts for db in liquibase
    - Created models

---
+++ backend/src/main/java/dev/roland/billing_program/models/User.java
@@ -0,0 +1,35 @@
+package dev.roland.billing_program.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private LocalDateTime lastLogin;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+}
